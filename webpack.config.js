41d69a7b0f90fdae1cf19747a8f13f862674abbc
richard <richard.ytli+f@gmail.com>
2022-05-31 18:30:19 +0800

    [styling] project list; project item buttons visible only on active + hover

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
